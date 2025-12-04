e10fc0be654634a8ad6829d4d3e0fa4d46f5f622
EanAnciso <167955538+EanAnciso@users.noreply.github.com>
2025-12-04 12:22:53 -0500

    Add necessary QMD file and R file
    This commit will add the necessary QMD file that will eventually contain all of our EDA and reporting. It also adds an R file that we will be using to organize and test various coding.

---
+++ CodeSpace.R
@@ -0,0 +1,3 @@
+# Coding File
+# This document will be used to organize and work on the specific code of each of our objectives
+# All necessary code will transferred to the qmd document when thoroughly tested and approved
